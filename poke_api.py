782794c8d1c6995aca1f5c009dd746d0fe48ef0d
Keyapatel3012 <Keyurip42@gmail.com>
2023-06-21 23:38:39 -0400

    Update poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -6,10 +6,11 @@ import requests
 
 POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
+
 def main():
     # Test out the get_pokemon_info() function
     # Use breakpoints to view returned dictionary
-    poke_info = get_pokemon_info("Rockruff")
+    poke_info = get_pokemon_info("PIKACHU")
     return
 
 def get_pokemon_info(pokemon_name):
@@ -21,13 +22,23 @@ def get_pokemon_info(pokemon_name):
     Returns:
         dict: Dictionary of Pokemon information, if successful. Otherwise None.
     """
-    # TODO: Clean the Pokemon name parameter
+    # Clean the Pokemon name parameter
+    pokemon_name = str(pokemon_name).strip().lower()
+
+    print(f"Getting information for {pokemon_name}...")
 
     # TODO: Build a clean URL and use it to send a GET request
+    poke_url = f"{POKE_API_URL}{pokemon_name}"
+    resp_msg = requests.get(poke_url)
 
-    # TODO: If the GET request was successful, convert the JSON-formatted message body text to a dictionary and return it
+    # If the GET request was successful, convert the JSON-formatted message body text to a dictionary and return it
+    if resp_msg.ok:
+        return resp_msg.json()
+    else:
 
-    # TODO: If the GET request failed, print the error reason and return None
+    # If the GET request failed, print the error reason and return None
+     print('failure')
+     print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})') 
 
     return
 
